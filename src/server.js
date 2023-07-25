36bcab666481f04aa11b46dde9e6a24432c2bde7
KingJ <smaragdinesapphire@gmail.com>
2023-07-25 17:11:22 +0800

    feat: support http reqeust

--- src/server.js
+++ src/server.js
@@ -1,8 +1,18 @@
 const fs = require("fs").promises;
 const WebSocket = require("ws");
 const path = require("path");
+const express = require("express");
+const cors = require("cors");
+const PORT = 8080;
 
-const wss = new WebSocket.Server({ port: 8080 });
+const app = express();
+app.use(cors());
+
+const server = app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
+
+const wss = new WebSocket.Server({ server });
 
 const writeToFile = (path, data) => {
   const jsonStr = JSON.stringify(data);
@@ -33,7 +43,9 @@ wss.on("connection", (ws) => {
           writeToFile(path.join(__dirname, "../data/data.log"), data);
           break;
         case "load":
-          readFile(path.join(__dirname, "../data/data.log")).then((contents) => ws.send(contents));
+          readFile(path.join(__dirname, "../data/data.log")).then((contents) =>
+            ws.send(contents)
+          );
           break;
         default:
           throw Error();
@@ -46,4 +58,28 @@ wss.on("connection", (ws) => {
   ws.on("close", () => {
     console.log("Client disconnected.");
   });
+});
+
+/**
+ * http server
+ */
+app.post("/save", (req, res) => {
+  console.log("http save");
+  req.on("data", (data) => {
+    return writeToFile(path.join(__dirname, "../data/data.log"), JSON.parse(data));
+  });
+});
+
+app.get("/load", (req, res) => {
+  console.log("http load");
+  readFile(path.join(__dirname, "../data/data.log"))
+    .then((contents) => {
+      res.status(200).json(contents);
+    })
+    .catch((e) => {
+      console.log(e);
+      res
+        .status(500)
+        .end(e.code === "ENOENT" ? "no record" : "something error");
+    });
 });
